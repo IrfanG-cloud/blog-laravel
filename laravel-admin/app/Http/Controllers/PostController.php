4b90585a99e173bfd2c125ee85d81dc584cacfeb
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-11-11 13:35:55 +0400

    migration,factory, seed updated forpost

--- laravel-admin/app/Http/Controllers/PostController.php
+++ laravel-admin/app/Http/Controllers/PostController.php
@@ -3,35 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(){
-
-        $post = [
-            "id" => 1,
-            "title" => "Dr. Jazmyne Wilderman DVM",
-            "description" => "Ellis Bahringer DVM",
-            "created_at" => "2023-11-02T07:50:10.000000Z",
-            "updated_at" => "2023-11-02T07:50:10.000000Z"
-        ];
-
-        return $post;
+        return Post::all();
     }
+
 
     public function show($id){
-
-        $post = [
-            "id" => 2,
-            "title" => "Dr. Jazmyne Wilderman DVM",
-            "description" => "Ellis Bahringer DVM",
-            "created_at" => "2023-11-02T07:50:10.000000Z",
-            "updated_at" => "2023-11-02T07:50:10.000000Z"
-        ];
-
-        return $post;
+        return User::find($id);
     }
+
+
 
     public function store(Request $request){
         $post = [
