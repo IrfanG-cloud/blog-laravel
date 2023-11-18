00e97bfc810c2bb35623e09bc6c7d62353ea5d6e
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-11-18 10:51:12 +0400

    updated

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 
 Route::post('posts', 'App\Http\Controllers\AuthController@login');
-Route::group(['middleware' => 'auth:api'], function () {
 
 
 Route::group(['middleware' => 'auth:api'], function () {
