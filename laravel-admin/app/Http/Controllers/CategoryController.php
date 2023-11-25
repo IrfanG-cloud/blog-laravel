9ad4a740f3a8fc6f2142db9d5b91ef10a8ce5aa7
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-11-25 23:05:08 +0400

    categories api tested

--- laravel-admin/app/Http/Controllers/CategoryController.php
+++ laravel-admin/app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -17,14 +19,29 @@ class CategoryController extends Controller
 
 
     public function store(Request $request){
+        $category = Category::create([
+            'title' => $request->input('title'),
+        ]);
+    return response($category);
 
     }
 
     public function update(Request $request, $id){
+        $category = Category::find($id);
+
+        $category->update([
+                'id' => $request->input('id'),
+                'title' => $request->input('title'),
+            ]);
+
+        return response($category);
         
     }
 
     public function destroy(Request $request, $id){
+        Category::destroy($id);
+
+        return response(null);
         
     }
 }
